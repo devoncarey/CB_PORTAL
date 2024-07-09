83cd34834dade3231afc09a01544473717be3a4f
devoncarey <dccrossventuresllc@gmail.com>
2024-07-08 23:32:20 -0400

    updates files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # devoncarey.com
  main entrance for devon carey's experiences and projects
+
+/devoncarey
+│
+├── /assets
+│   ├── /images
+│   └── /videos
+│
+├── /css
+│   └── style.css
+│
+├── /js
+│   └── script.js
+│
+├── index.html
+├── .gitignore
+└── README.md
---
+++ script.js
@@ -0,0 +1,12 @@
+// js/script.js
+document.getElementById('fearful-symmetry').onclick = function() {
+    window.location.href = 'https://www.fearfulsymmetry.com';
+};
+
+document.getElementById('alderwood-origins').onclick = function() {
+    window.location.href = 'https://www.alderwoodorigins.com';
+};
+
+document.getElementById('escaping-alderwood').onclick = function() {
+    window.location.href = 'https://www.escapingalderwood.com';
+};
